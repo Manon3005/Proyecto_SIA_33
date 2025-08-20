a7776be02280a787fec3656a986c7db9cf4908ff
Manon3005 <manon.b2003@gmail.com>
2025-08-20 13:25:49 -0400

    init

---
+++ mi-proyecto/src/main/java/com/mi/proyecto/MiProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mi.proyecto;
+
+/**
+ *
+ * @author manon
+ */
+public class MiProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
