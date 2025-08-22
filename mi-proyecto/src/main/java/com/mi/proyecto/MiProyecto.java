ff08562bdd669aaf4e76fd8235f71316738a8701
Manon3005 <manon.b2003@gmail.com>
2025-08-22 09:18:20 -0400

    fix: eliminar duplicado

--- mi-proyecto/src/main/java/com/mi/proyecto/MiProyecto.java
+++ mi-proyecto/src/main/java/com/mi/proyecto/MiProyecto.java
@@ -4,6 +4,7 @@
 
 package com.mi.proyecto;
 
+import com.mi.proyecto.repositories.SucursalRepository;
 import com.mi.proyecto.repositories.ClienteRepository;
 import com.mi.proyecto.repositories.EmpleadoRepository;
 import com.mi.proyecto.repositories.TicketRepository;
@@ -17,13 +18,13 @@ public class MiProyecto {
     private ClienteRepository clienteRepo;
     private EmpleadoRepository empleadoRepo;
     private TicketRepository ticketRepo;
-    private SucursalRespository sucursalRepo;
+    private SucursalRepository sucursalRepo;
     
     public MiProyecto() {
         this.clienteRepo = new ClienteRepository();
         this.empleadoRepo = new EmpleadoRepository();
         this.ticketRepo = new TicketRepository();
-        this.sucursalRepo = new SucursalRespository();
+        this.sucursalRepo = new SucursalRepository();
         
     }
 
