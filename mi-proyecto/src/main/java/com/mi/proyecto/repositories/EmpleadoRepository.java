5df50bcd3d657e4cdbf689038758f551e74b5b0e
Byron-Vasquez <byronvasquez430@gmail.com>
2025-08-25 00:12:52 -0400

    Update 1.1 EmpleadoRepository.java
    Se agrego metodos y se agrego unos comentarios de posibles metodos nuevos

--- mi-proyecto/src/main/java/com/mi/proyecto/repositories/EmpleadoRepository.java
+++ mi-proyecto/src/main/java/com/mi/proyecto/repositories/EmpleadoRepository.java
@@ -26,5 +26,61 @@ public class EmpleadoRepository {
     public void setEmpleados(List<Empleado> empleados) {
         this.empleados = empleados;
     }
+
+    // funicion que verifica y agrega un empleado que no este en la lista
+    public boolean agregarEmpleado(Empleado empleado){
+        if(buscarEmpleadoPorRut(empleado.getRut())!=null){
+           return false; // Ya existe un empleado con el mismo rut 
+        }
+        return empleados.add(empleado);
+    }
+    
+    public Empleado buscarEmpleadoPorRut(String rut){
+        for(Empleado emp: empleados){
+         if(emp.getRut().equals(rut)){
+             return emp;
+         }   
+        }
+        return null;
+    }
+    
+    public Empleado empleadoConMasTickets(){
+        if(empleados.isEmpty()){
+            System.out.println("No hay empleados registrados");
+           return null;
+        }
+        
+        Empleado maxEmpleado = empleados.get(0);
+        int maxTickets = maxEmpleado.getTickets()!= null ? maxEmpleado.getTickets().size() : 0;
+        
+        for(Empleado emp : empleados){
+          if(emp!=null){
+            int cantTickets = emp.getTickets() != null ? emp.getTickets().size() : 0;
+            
+            if(cantTickets > maxTickets){
+                maxTickets = cantTickets;
+                maxEmpleado = emp;
+            }    
+           }
+        }
+        
+        // Verificar si al menos un empleado tiene tickets
+        if(maxTickets == 0){
+            System.out.println("Ningun empleado tiene tickets asignados");
+            return null;
+        }
+        
+        return maxEmpleado;  
+    }
     
 }
+  /**
+     * ???
+     * 1. generar reporte de tickets del empleado
+     * 2. generar reporte de los empleados
+     * 3. cambiar estado de los tickets
+     * 4. agregar empleado pero que se verifique que no esta en la lista
+     * 5. quiza hacer una funcion de reasignar el ticket a un empleado que tenga menos trabajo 
+     * ???
+     */
+    
